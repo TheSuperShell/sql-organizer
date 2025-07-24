b49d8855df257832c4e50a02d69530d9a340a0e5
Steven Rudenko <steven.gucum@gmail.com>
2025-07-24 22:25:16 +0300

    Add formatting to the combiner

--- src/sql_organizer_file_combiner/combiner.py
+++ src/sql_organizer_file_combiner/combiner.py
@@ -2,8 +2,11 @@ from collections.abc import Sequence
 from pathlib import Path
 
 from sql_organizer_file.file import SqlFile
+from sql_organizer_file_formatter.file_formatter import SqlFileFormatter
 
 
-def combine_files(new_file_path: Path, files: Sequence[SqlFile]):
+def combine_files(
+    new_file_path: Path, files: Sequence[SqlFile], formatter: SqlFileFormatter
+):
     with new_file_path.open("w") as f:
-        f.write("\n".join(f.sql_text for f in files))
+        f.write("\n".join(formatter.format_file(f) for f in files))
