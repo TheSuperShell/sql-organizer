64342dfbead4fce8ea6f28bccc6b47a22926d3f9
Steven Rudenko <steven.gucum@gmail.com>
2025-07-24 18:14:22 +0300

    add a simple file search engine

--- src/sql_organizer/main.py
+++
@@ -1,9 +0,0 @@
-from typing import Annotated
-
-import typer
-
-EchoMessage = Annotated[str, typer.Argument(help="The echo message")]
-
-
-def echo(message: EchoMessage) -> None:
-    print(message)
---
+++ src/sql_organizer_cli/main.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from typing import Annotated
+
+import rich
+import rich.errors
+import typer
+from pydantic import ValidationError
+
+from sql_organizer_search_engine.search import FileExtension, get_all_sql_files
+
+app = typer.Typer()
+
+
+@app.command()
+def echo(message: Annotated[str, typer.Argument(help="Message to echo")]) -> None:
+    rich.print(message)
+    print("done")
+
+
+@app.command()
+def search(
+    path: Annotated[str, typer.Argument(help="search path")] = ".",
+    extension: Annotated[list[str], typer.Option("--extension", "-e")] = ["sql"],
+) -> None:
+    search_path = Path(path)
+    try:
+        extensions = [FileExtension(extension=e) for e in extension]
+    except ValidationError:
+        rich.print(
+            "[bold red]Value Error![/bold red] Extension should not be an empty string"
+        )
+        return
+    files = get_all_sql_files(search_path, extensions)
+    for file in files:
+        rich.print(file.absolute())
